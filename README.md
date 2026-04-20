e9877e82359f891f9c0c43cfffc1dddaa1679cdd
Rogerio Weirich <rogeriorweirich@gmail.com>
2026-04-20 19:52:12 -0300

    feat: initial project setup, README and superclass Product

---
+++ README.md
@@ -0,0 +1,43 @@
+## 🍔 Custom Food Management System 
+
+A robust order management system for highly customizable meals, inspired by the "build-your-own" logic (similar to Subway). Developed in Java, focusing on Clean Code, Object-Oriented Programming (OOP) principles, and scalable architecture.
+
+### 🚀 Project Status 
+In Progress: Developing the core Domain Model and Business Logic.
+
+🗺️ Development Roadmap:
+
+* [✔️] Product Superclass (Base structure, stock management, and pricing contract)
+
+* [  ] Ingredient/Addon Enum (Categorized ingredients for customization)
+
+* [  ] Subclasses Implementation (Hamburger, Sandwiches, Sides, Desserts)
+
+* [  ] Customization Logic (Addon integration and final price calculation)
+
+* [  ] Drink & Combo Systems (Implementation of beverages and special pricing)
+
+* [  ] MySQL Persistence Layer (Database schema, DDL/DML, and JDBC)
+
+* [  ] CLI User Interface (Interactive console-based ordering)
+
+## 🛠️ Tech Stack & Concepts
+
+- Language: Java 21
+
+- OOP Patterns: Polymorphism, Inheritance, Composition, and Interfaces.
+
+- Database: MySQL (Persistence, Security, and Access Control).
+
+- Error Handling: Custom Business Exceptions.
+
+#####  📂 Project Structure:
+
+```
+custom-food-system/
+│   └── src/
+│       └── model/
+│           └── Product.java
+├── .gitignore
+└── README.md
+```
---
+++ src/model/Product.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class Product {
+    private final String id;
+    private String name;
+    private double basePrice;
+    private String description;
+    private int stockQuantity;
+
+    protected final List<Ingredient> addons;
+
+    public Product(String name, double basePrice, String description, int initialStock) {
+        this.id = UUID.randomUUID().toString(); //random unique ID
+        this.name = name;
+        this.basePrice = basePrice;
+        this.description = description;
+        this.stockQuantity = initialStock;
+        this.addons = new ArrayList<>();
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public List<Ingredient> getAddons() {
+        return Collections.unmodifiableList(addons);
+    }
+
+    //Business rule
+    public abstract double calculateFinalPrice();
+
+    public void addIngredient(Ingredient ingredient) {
+        if (ingredient != null) {
+            this.addons.add(ingredient);
+        }
+    }
+
+    // Stcok management
+    public boolean isAvailable() {
+        return stockQuantity > 0;
+    }
+
+    public void decreaseStock(int quantity) {
+        if (quantity > 0 && quantity <= stockQuantity) {
+            stockQuantity -= quantity;
+        }
+    }
+
+    protected double calculateAddonsPrice() {
+        return addons.stream()
+                .mapToDouble(Ingredient::getPrice)
+                .sum();
+    }
+
+    public String getDetails() {
+        String shortId = id.substring(0, 8);
+        return String.format("ID: [%s] | Product: %s | Price: R$ %.2f | Description: %s",
+                shortId, name, calculateFinalPrice(),description);
+    }
+}
