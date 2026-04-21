f3113c62ac3c641212f16e0d3a799377b42d14c0
Rogerio Weirich <rogeriorweirich@gmail.com>
2026-04-21 19:56:02 -0300

    feat: add Beverage subclass for for drink items

---
+++ src/model/Beverage.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Beverage extends Product {
+    public Beverage(String name, double basePrice, String description, int initialStock) {
+        super(name, basePrice,description,initialStock);
+    }
+
+    /**
+     * Business rule to calculate the implemantation of Beverage
+     * Base Price + any extra that the client would want to add
+     */
+    @Override
+    public double calculateFinalPrice() {
+        return getBasePrice() + calculateAddonsPrice();
+    }
+
+
+    @Override
+    public String getDetails() {
+        StringBuilder details = new StringBuilder(super.getDetails());
+
+        if (!getAddons().isEmpty()) {
+            details.append("\n -> Extras Beverage: ");
+
+            getAddons().forEach(addon ->
+                    details.append(addon.getDisplayName()).append(", ")
+            );
+            details.setLength(details.length() - 2);
+        }
+        return details.toString();
+    }
+}
