349118daa9b9af2bfc94eaca57287456261ec7a6
Rogerio Weirich <rogeriorweirich@gmail.com>
2026-04-21 21:37:38 -0300

    feat: add Sandwich subclass for submarine style sandwiches

---
+++ src/model/Sandwich.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Sandwich extends Product {
+    public Sandwich(String name, double basePrice, String description, int initialStock) {
+        super(name, basePrice, description, initialStock);
+    }
+
+    /**
+     * Implementation of the pricing rule: Base price + Extra Filling price
+     */
+    @Override
+    public double calculateFinalPrice() {
+        return getBasePrice() + calculateAddonsPrice();
+    }
+
+    @Override
+    public String getDetails() {
+        StringBuilder details = new StringBuilder(super.getDetails());
+
+        if (!getAddons().isEmpty()) {
+            details.append("\n -> Fillings and Extras: ");
+
+            getAddons().forEach(addon ->
+                details.append(addon.getDisplayName()).append(", ")
+            );
+
+            details.setLength(details.length() - 2);
+        }
+
+        return details.toString();
+    }
+}
