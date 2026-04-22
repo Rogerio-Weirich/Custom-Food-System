0137e7349eb3d9c57a22ab125b739204d3dc7fb4
Rogerio Weirich <rogeriorweirich@gmail.com>
2026-04-21 22:10:18 -0300

    refactor: expand Ingredient enum with sandwich and hot dog items

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -1,42 +1,64 @@
 package model;
 
 public enum Ingredient {
-    // Base meats (no initial extra cost for the standard setup)
+    // Base buns (no extra cost)
+    BUN("Bun", 0.0),
+    WHOLE_WHEAT_BUN("Whole Wheat Bun", 0.0),
+    BAGUETTE("Baguette Bread", 0.0),
+    HOT_DOG_BUN("Hot Dog Bun", 0.0),
+
+    // Base Hamburgers (no initial extra cost for the standard setup)
     BEEF_PATTY("Beef Patty", 0.0),
     CHICKEN_PATTY("Chicken Patty", 0.0),
     PORKY_PATTY("Porky Patty", 0.0),
     VEGGIE_PATTY("Veggie Patty", 0.0),
 
-    // Base buns (no extra cost)
-    BUN("Bun", 0.0),
-    WHOLE_WHEAT_BUN("Whole Wheat Bun", 0.0),
+    // Base Sausages
+    PORK_SAUSAGE("Pork Sausage", 0.0),
+    CHICKEN_SAUSAGE("Chicken Sausage", 0.0),
+    VEGGIE_SAUSAGE("Veggie Sausage", 1.50),
+
+    // Base Sandwich
+    CHICKEN_BREAST("Chicken Breast", 0.0),
+    TURKEY_BREAST("Turkey Breast", 0.0),
+    ROAST_BEEF("Roast Beef", 0.0),
+    HAM("Sliced Ham", 0.0),
+    MORTADELLA("Sliced Mortadella", 0.0),
 
     // Salads (no extra cost)
     LETTUCE("Lettuce", 0.0),
     TOMATO("Tomato", 0.0),
     ONION("Onion", 0.0),
     PICKLES("Pickles", 0.0),
+    CORN_PEAS("Corn & Peas", 0.0),
+    OLIVES("Black Olives", 0.50),
 
     // Paid addons (Cheeses, extra meats, etc)
-    CHEESE("Cheese", 2.50),
-    BACON("Bacon", 3.50),
+    SALAMI("Salami", 1.50),
+    MOZZARELLA("Mozzarella Cheese", 2.00),
+    CHEDDAR_CHEESE("Chedadr Cheese", 2.50),
+    SWISS_CHEESE("Swiss Cheese", 3.00),
+    PROVOLONE("Provolone Cheese", 3.00),
     EGG("Egg", 2.00),
-    DOUBLE_PATTY("Double Patty", 6.00),
+    BACON("Bacon", 3.50),
+    DOUBLE_MEAT("Double Meat", 4.50), // Sandiwch
+    DOUBLE_SAUSAGE("Double Sausage", 5.00), // Wiener
+    DOUBLE_PATTY("Double Patty", 6.00), // Hamburger
     ONION_RINGS("Onion Rings", 4.50),
 
     // Extra addons (For Sides)
     CHEDDAR_SAUCE("Cheddar Suace", 3.00),
     BACON_BITS("Bacon Bits", 2.50),
     PARMESAN("Grated Parmesan", 2.00),
-    GARLIC_MAYONNAISE("Garlic Mayonnaise Dip", 1.50),
 
     // Sauces (few free, some paid)
     KETCHUP("Ketchup", 0.0),
     MUSTARD("Mustard", 0.0),
     MAYONNAISE("Mayonnaise", 0.0),
     SPICY_SAUCE("Spicy Sauce", 1.00),
-    GREEN_SAUCE("Green Sauce", 1.25),
+    GARLIC_MAYONNAISE("Garlic Mayonnaise Dip", 1.50),
     GARLIC_SAUCE("Garlic Sauce", 1.50),
+    GREEN_SAUCE("Green Sauce", 1.25),
 
     // Extras and Coverages
     ICE_CREAM_SCOOP("Ice Cream Scoop", 3.50),
