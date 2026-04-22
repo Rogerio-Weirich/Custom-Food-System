c8586885863cb8fc6ca42a0ddd8892520358a465
Rogerio Weirich <rogeriorweirich@gmail.com>
2026-04-21 21:12:26 -0300

    feat: add Side subclass for side dishes

---
+++ src/model/Side.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Side extends Product {
+    public Side(String name, double basePrice, String description, int initalStock) {
+        super(name, basePrice,description,initalStock);
+    }
+
+    /**
+     * Mandatory implementation of the pricing rule.
+     * Add up the base price of the side dish to any extra charges added.
+     */
+    @Override
+    public double calculateFinalPrice() {
+        return getBasePrice() + calculateAddonsPrice();
+    }
+
+    @Override
+    public String getDetails() {
+        StringBuilder details = new StringBuilder(super.getDetails());
+
+        // Verify is there is addons (like bacon, etc.)
+        if (!getAddons().isEmpty()) {
+            details.append("\n -> Extra Sides: ");
+
+            getAddons().forEach(addon ->
+                details.append(addon.getDisplayName()).append(", ")
+            );
+
+            details.setLength(details.length() - 2);
+        }
+
+        return details.toString();
+    }
+}
