1a8e6ea53752807a554975de4635fef2c72dc3c5
Rogerio Weirich <rogeriorweirich@gmail.com>
2026-04-21 21:49:22 -0300

    feat: add Wiener subclass for hot dog products

---
+++ src/model/Wiener.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Wiener extends Product {
+    public Wiener(String name, double basePrice, String description, int initialStock) {
+        super(name, basePrice, description, initialStock);
+    }
+
+    @Override
+    public double calculateFinalPrice() {
+        return getBasePrice() + calculateAddonsPrice();
+    }
+
+    @Override
+    public String getDetails() {
+        StringBuilder details = new StringBuilder(super.getDetails());
+
+        if (!getAddons().isEmpty()) {
+            details.append("\n -> Sausages and Extras: ");
+
+            getAddons().forEach(addon ->
+                details.append(addon.getDisplayName()).append(", ")
+            );
+
+            details.setLength(details.length() - 2);
+        }
+
+        return details.toString();
+    }
+}
